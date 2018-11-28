28926592361b825956583709619581f075d9124d
Nathan Arthur <nathan@simpleupdates.com>
2018-11-28 18:35:19 -0500

    git wip

---
+++ test/stubs/StubGit.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ThemeViz;
+
+class StubGit extends Git
+{
+    use Stub;
+
+    public function saveState($path)
+    {
+        return $this->handleCall(__FUNCTION__, func_get_args());
+    }
+
+    public function resetState($path)
+    {
+        return $this->handleCall(__FUNCTION__, func_get_args());
+    }
+
+    public function checkoutRemoteBranch($path, $branch)
+    {
+        return $this->handleCall(__FUNCTION__, func_get_args());
+    }
+
+    public function pull($path, $branch)
+    {
+        return $this->handleCall(__FUNCTION__, func_get_args());
+    }
+}
