1fab4150df75bb6405db6029715eb263f04fe5f0
Nathan Arthur <nathan@simpleupdates.com>
2018-11-30 09:03:25 -0500

    Add circle-github-bot & comment script

---
+++ comment.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+const bot = require("third-party/circle-github-bot-release-1.0.0").create();
+
+bot.comment(`
+<h3>${bot.env.commitMessage}</h3>
+Component diffs: <strong>${bot.artifactLink('app/build/summary.html', 'Summary')}</strong>
+`);
